9f5fd3d2aee2f1f06ed6bd50b654c1042cdeb163
Rustam Azimbaev <rustam.azimbaev365@gmail.com>
2020-04-08 19:48:01 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Simple Chatroom Apps
+
+For the end result this app should support the following:
+
+v1:
+
+1. Chat should display most recent 500 messages or so
+2. Use websocket for real time duplex communication between server and client
+3. Store chat in s3 (maybe broken up into different blocks) and store metadata in either DynamoDB or RDB
+
+v2:
+
+4. Multiple chatrooms by topic (Topics will be predefined and NOT dynamically created by user)
+5. Use Caching for better performance (maybe Redis)
+6. Users should be able to register for account
+7. Users should be able to post with or without account. If posting with account then display their username in messages
---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,47 @@
+const webpack = require("webpack");
+const path = require("path");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map",
+  entry: "./src/index",
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
+  output: {
+    path: path.resolve(__dirname, "..", "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true,
+    historyApiFallback: true,
+    // disableHostCheck: true,
+    // headers: { "Access-Control-Allow_Origin": "*" },
+    // https: false
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: "./src/index.html",
+      //   favicon: "../src/favicon.ico"  // TODO - get a favicon and uncomment this line
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"],
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};

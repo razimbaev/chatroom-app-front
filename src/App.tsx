6449735253a3b6719a1ddadd6591025e61d32be3
Rustam Azimbaev <rustam.azimbaev365@gmail.com>
2020-04-23 17:57:23 -0400

    Remove commented out code

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,6 @@ import WebSocketProvider from "./components/WebSocketContext";
 import "./index.css";
 
 const App = () => {
-  // return (
-  //   <div className="talk-bubble tri-right round right-in">
-  //     <div className="talktext">
-  //       <p>
-  //         CSS Talk Bubble configured by classes. Defaults to square shape, no
-  //         triangle. Height is auto-adjusting to the height of the text.
-  //       </p>
-  //     </div>
-  //   </div>
-  // );
   return (
     <WebSocketProvider>
       <Routes />

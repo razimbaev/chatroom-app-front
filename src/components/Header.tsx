6a27c5b74110843b672e70f6fb079f9da7f65af1
Rustam Azimbaev <rustam.azimbaev365@gmail.com>
2020-04-23 10:52:52 -0400

    Remove login and signup because will not support that for now

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
-import LoginModal from "./LoginModal";
-import SignupModal from "./SignupModal";
 
 const Header = () => {
-  // TODO - see if there's a better way then to declare state here and pass it to Modal Components (maybe use redux to manage show state globally)
-  const [showLoginModal, setShowLoginModal] = useState(false);
-  const handleLoginModalOpen = () => {
-    setShowLoginModal(true);
-  };
-  const handleLoginModalClose = () => {
-    setShowLoginModal(false);
-  };
-
-  const [showSignupModal, setShowSignupModal] = useState(false);
-
-  const handleSignupModalOpen = () => {
-    setShowSignupModal(true);
-  };
-  const handleSignupModalClose = () => {
-    setShowSignupModal(false);
-  };
-
   return (
     <Navbar bg="dark" variant="dark" className="header-bar">
       <Navbar.Brand as={Link} to="/">
         MyChatRoom
       </Navbar.Brand>
       <Nav className="mr-auto">
-        <Nav.Link as={Link} to="/chatroom/2k">
+        <Nav.Link as={Link} to="/">
           Explore
         </Nav.Link>
         <Form inline>
@@ -45,26 +24,13 @@ const Header = () => {
         </Form>
       </Nav>
       <Nav className="ml-auto">
-        <Nav.Link as={Link} to="/chatroom/2k">
+        <Nav.Link as={Link} to="/">
           Random Chatroom
         </Nav.Link>
-        <Nav.Link as={Link} to="/chatroom/2k">
+        <Nav.Link as={Link} to="/">
           My Chats
         </Nav.Link>
-        <NavDropdown title="Account" id="basic-nav-dropdown">
-          <NavDropdown.Item onClick={handleSignupModalOpen}>
-            Sign Up
-          </NavDropdown.Item>
-          <NavDropdown.Item onClick={handleLoginModalOpen}>
-            Login
-          </NavDropdown.Item>
-        </NavDropdown>
       </Nav>
-      <LoginModal show={showLoginModal} handleClose={handleLoginModalClose} />
-      <SignupModal
-        show={showSignupModal}
-        handleClose={handleSignupModalClose}
-      />
     </Navbar>
   );
 };
